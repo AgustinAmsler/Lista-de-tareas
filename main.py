d3713b76050dd49f482a2c01e05e5f271d639dbf
Agustin Amsler <amsleragustin@gmail.com>
2024-09-02 16:58:46 -0300

    Test agrega 1 tarea a una lista de tareas e implementacion

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ class Tarea:
     def comprobar_hora_limite(self, hora_limite):
         return self.__hora_limite == hora_limite
 
+
+class Lista_de_tareas:
+    def __init__(self):
+        self.__lista_de_tareas = []
+
+    def agregar_tarea(self, tarea):
+        self.__lista_de_tareas.append(tarea)
+
+    def comprobar_cantidad_de_tareas(self, numero):
+        return len(self.__lista_de_tareas) == numero
+
--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import pytest
-from main import Tarea
+from main import Tarea, Lista_de_tareas
 
 
 def test_crear_tarea():
@@ -8,3 +8,12 @@ def test_crear_tarea():
     assert tarea.comprobar_titulo("Hacer la cama")
     assert tarea.comprobar_descripcion("Hacer la cama despues de despertarme")
     assert tarea.comprobar_hora_limite("08:00:00")
+
+
+def test_agregar_1_tarea_a_una_lista_de_tareas():
+        
+    lista_de_tareas = Lista_de_tareas()
+    tarea = Tarea("Hacer la cama", "Hacer la cama despues de despertarme", "08:00:00")
+    lista_de_tareas.agregar_tarea(tarea)
+    assert lista_de_tareas.comprobar_cantidad_de_tareas(1)
+
